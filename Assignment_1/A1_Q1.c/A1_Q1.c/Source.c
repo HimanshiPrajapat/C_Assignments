f511b1cc72e23fb54396befbddabaec9feafce8c
HimanshiPrajapat <115346167+HimanshiPrajapat@users.noreply.github.com>
2022-10-15 06:40:47 -0700

    Add files via upload

---
+++ Assignment_1/A1_Q1.c/A1_Q1.c/Source.c
@@ -0,0 +1,12 @@
+/*Author: Himanshi prajapat
+Date: 26-sept-2022
+Aim: print "Hello" on screen
+Language: c
+*/
+#include<stdio.h>
+int main()
+{
+	printf("Hello");
+	//My first line written in c language
+	return 0;
+}
